80f3cd6c3990fad9a0d60872a9e77d92e9f6748d
Martin Kiprotich. Ruto <MRUTO@safaricom.co.ke>
2018-04-04 10:59:40 +0300

    add start screen

--- app/src/main/java/com/example/mruto/cashflow/view/RatRaceActivity.java
+++ app/src/main/java/com/example/mruto/cashflow/view/RatRaceActivity.java
@@ -1,4 +1,4 @@
-package com.example.mruto.cashflow;
+package com.example.mruto.cashflow.view;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,14 +13,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+import com.example.mruto.cashflow.R;
+import com.example.mruto.cashflow.viewmodel.NavigationViewClass;
+
+public class RatRaceActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_rat_race);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -79,21 +81,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+        NavigationViewClass navigationViewClass = new NavigationViewClass(this);
+        navigationViewClass.onNavigationItemSelected(item);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
